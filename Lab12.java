aa621141066e46a893dcd6d653186f6997315d72
ShahzaibShah24 <156691876+ShahzaibShah24@users.noreply.github.com>
2024-01-16 09:33:01 +0500

    Add files via upload

---
+++ Lab12.java
@@ -0,0 +1,17 @@
+package lab12;
+
+import java.util.Scanner;
+
+public class Lab12 {
+	public static void main(String[] args) {
+		try (Scanner input = new Scanner(System.in)) {
+			int[] array = { 4, 11, 13, 34, 312, 6, 10, 44, 25, 27 };
+			System.out.print("Enter the index of Array : ");
+			try {
+				System.out.println("The Value Is : " + array[input.nextInt()]);
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("Array Index Out Of Bounds. ");
+			}
+		}
+	}
+}
